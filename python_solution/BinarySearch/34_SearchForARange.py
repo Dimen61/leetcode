91e2dbb85f4a3dbb24b12e6c0bff959487861f60
Mao Ding <dimen61@gmail.com>
2016-11-29 09:39:34 +0800

    Use a universal binary search to refactor 34

--- python_solution/BinarySearch/34_SearchForARange.py
+++ python_solution/BinarySearch/34_SearchForARange.py
@@ -31,6 +31,64 @@ class Solution(object):
                 left = mid
         end_pos = left
         return [start_pos, end_pos]
+
+    def searchRange(self, nums, target):
+        """
+        A better and universal binary search solution.
+
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # Find the start position
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = (left+right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] == target:
+                if mid > 0 and nums[mid-1] == target:
+                    right = mid - 1
+                else:
+                    right = mid
+
+        if nums[left] != target: return [-1, -1]
+        start_pos = left
+
+        # Find the end position
+        left, right = 0, len(nums)-1
+        while left < right:
+            mid = (left+right) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] == target:
+                if mid+1 < len(nums) and nums[mid+1] == target:
+                    left = mid + 1
+                else:
+                    left = mid
+                    break
+        end_pos = left
+        return [start_pos, end_pos]
+
                 
 a = Solution()
 print(a.searchRange([1], 1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
