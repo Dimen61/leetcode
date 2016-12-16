0c524787a1e05302a13754602c20a8486d425905
Mao Ding <dimen61@gmail.com>
2016-12-16 18:16:46 +0800

    add readme

--- python_solution/BitManipulation/371_SumOfTwoIntegers.py
+++ python_solution/BitManipulation/371_SumOfTwoIntegers.py
@@ -5,43 +5,15 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        res = 0
-        pow_2 = 1
-        cc = 0
-        while a > 0 or b > 0:
+        # 32 bits interger min
+        MAX_INT = 0x7FFFFFFF
+        # mask to get last 32 bits
+        mask = 0xFFFFFFFF
+        while b != 0:
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
 
-            print('a: {0}; b: {1}'.format(a, b))
-
-            aa = a & 0x1
-            bb = b & 0x1
-            res += pow_2 * (aa ^ bb ^ cc)
-            cc = (aa & bb) | (aa & cc) | (bb & cc)
-            pow_2 <<= 1
-            
-            a >>= 1
-            b >>= 1
-        if cc > 0:
-            res += pow_2
-        return res
-
-    def getSum(self, a, b):
-        """
-        :type a: int
-        :type b: int
-        :rtype: int
-        """
-        res = 0
-        pow_2 = 1
-        cc = 0
-        for i in range(32):
-            aa = pow_2 & a
-            bb = pow_2 & b
-            if aa ^ bb ^ cc > 0:
-                res |= pow_2
-            cc = (aa & bb) | (aa & cc) | (bb & cc)
-            pow_2 <<= 1
-        return res
+        return a if a <= MAX_INT else ~((a-1) ^ mask) + 1
 
 a = Solution()
-print(a.getSum(1, 3))
+print(a.getSum(-1, -5))
         
