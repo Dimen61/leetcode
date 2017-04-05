0ef314e48eb0ac7798f171af744ee074bc8143ae
Mao Ding <dimen61@gmail.com>
2017-04-05 14:31:50 +0800

    add 11

---
+++ python_solution/TwoPointers/11_ContainerWithMostWater.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        max_area = 0
+        while left < right:
+            max_area = max(max_area, (right - left) * min(height[left], height[right]))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
+a = Solution()
+print(a.maxArea([1, 2, 4, 3]))
